ca149cd5c789647e9dab3acb16c83c627d93c59b
leviitta <av.diazh@gmail.com>
2019-10-05 22:23:46 -0300

    Termino de inicio de sesion con google y facebook

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,19 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart' as prefix0;
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 class User {
   User({@required this.uid});
   final String uid;
 }
 
-abstract class AuthBase{
+abstract class AuthBase {
   Stream<User> get onAuthStateChanged;
   Future<User> currentUser();
   Future<User> signInAnonymously();
+  Future<User> signInWithGoogle();
+  Future<User> signInWithFacebook();
   Future<void> signOut();
 }
 
-class Auth implements AuthBase{
+class Auth implements AuthBase {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   User _userFromFirebase(FirebaseUser user) {
@@ -23,7 +28,7 @@ class Auth implements AuthBase{
     return User(uid: user.uid);
   }
 
-  Stream<User> get onAuthStateChanged{
+  Stream<User> get onAuthStateChanged {
     return _firebaseAuth.onAuthStateChanged.map(_userFromFirebase);
   }
 
@@ -37,7 +42,46 @@ class Auth implements AuthBase{
     return _userFromFirebase(user);
   }
 
+  Future<User> signInWithGoogle() async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+
+    if (googleUser != null) {
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null && googleAuth.accessToken != null) {
+        FirebaseUser user = await _firebaseAuth.signInWithGoogle(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        );
+        return _userFromFirebase(user);
+      } else {
+        throw StateError('Missing Google Auth Token');
+      }
+    } else {
+      throw StateError('Google sign in aborted');
+    }
+  }
+
+  Future<User> signInWithFacebook() async {
+    final facebookLogin = FacebookLogin();
+    FacebookLoginResult result = await facebookLogin.logInWithReadPermissions(
+      ['public_profile'],
+    );
+    if (result.accessToken != null) {
+      FirebaseUser user = await _firebaseAuth.signInWithFacebook(
+        accessToken: result.accessToken.token,
+      );
+      return _userFromFirebase(user);
+    } else {
+      throw StateError('Missing Facebook access token');
+    }
+  }
+
   Future<void> signOut() async {
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
+    final facebookLogin = prefix0.FacebookLogin();
+    await facebookLogin.logOut();
     return await _firebaseAuth.signOut();
   }
 }
